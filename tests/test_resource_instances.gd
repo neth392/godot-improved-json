cfd5d25a5a177eb0d8fe3174ba459d541963449b
neth392 <cneth97@gmail.com>
2024-11-06 11:08:12 -0500

    some more unit tests for the weakref & resource instance system combo

--- tests/test_resource_instances.gd
+++ tests/test_resource_instances.gd
@@ -3,6 +3,7 @@ extends GutTest
 var impl: JSONSerializationImpl
 var config: JSONObjectConfig
 var resource_file_instance: JSONResourceFileInstance
+var weak_ref_resource_instance: Resource
 var resource: Resource
 
 func before_all() -> void:
@@ -10,13 +11,16 @@ func before_all() -> void:
 	impl.object_config_registry = JSONObjectConfigRegistry.new()
 	config = preload("./util/json_test_resource_config.tres")
 	impl.object_config_registry.add_config(config)
+	weak_ref_resource_instance = preload("./util/json_test_resource_weak_ref.tres")
 	resource = preload("./util/json_test_resource.tres")
+	resource.type_weak_ref_of_resource_instance = weakref(weak_ref_resource_instance)
 
 
 func after_all() -> void:
 	impl.queue_free()
 	impl = null
 	config = null
+	weak_ref_resource_instance = null
 	resource = null
 
 
@@ -58,3 +62,12 @@ func test_assert_deserialize_equals_original_via_path() -> void:
 	% deserialized)
 	
 	assert_eq(deserialized, resource, "deserialized != resource instance")
+
+
+func test_assert_deserialized_weakref_equals_original() -> void:
+	config.json_res_use_resource_path = true
+	var serialized: String = impl.stringify(resource)
+	var deserialized: Variant = impl.parse(serialized)
+	
+	assert_eq(deserialized.type_weak_ref_of_resource_instance.get_ref(), weak_ref_resource_instance,
+	"deserialized.type_weak_ref_of_resource_instance != weak_ref_resource_instance")
--- tests/util/json_test_resource.gd
+++ tests/util/json_test_resource.gd
@@ -4,3 +4,4 @@ class_name JSONTestResource extends Resource
 @export var type_float: float
 @export var type_int: int
 @export var type_sub_resource: JSONTestResource
+var type_weak_ref_of_resource_instance: WeakRef
